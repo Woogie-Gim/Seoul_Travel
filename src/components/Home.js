e629d768e3d638384dafc54b8dd2485e2433442d
김선욱 <tjsdnr960@naver.com>
2023-12-20 01:04:59 +0900

    서울 여행 정보 웹 서비스

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css';
+import '../css/Home.css';
 
 function Home() {
   const frames = useRef([]);
