0abe8063f4599a511dd4de97e2c3267f08f402a8
012coding <sugo2753@gmail.com>
2023-12-21 17:51:29 +0900

    축제 페이지 css 수정

--- src/components/Festival.js
+++ src/components/Festival.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import styles from "../css/Festival.module.css";
 
 function Festival() {
-  const [loading, setLoading] = useState(true);
   const [festival, setFestival] = useState([]);
   const [searchResult, setSearchResult] = useState([]);
   const [search, setSearch] = useState("");
@@ -38,6 +37,49 @@ function Festival() {
     "중랑구",
   ];
 
+  const placeholderImage =
+    "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg";
+
+  const ITEMS_PER_PAGE = 8;
+  const PAGINATION_NUMBERS = 5;
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPageCount = Math.ceil(searchResult.length / ITEMS_PER_PAGE);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const renderPaginationNumbers = () => {
+    const paginationNumbers = [];
+    const start =
+      Math.floor((currentPage - 1) / PAGINATION_NUMBERS) * PAGINATION_NUMBERS +
+      1;
+
+    for (
+      let i = start;
+      i < start + PAGINATION_NUMBERS && i <= totalPageCount;
+      i++
+    ) {
+      paginationNumbers.push(
+        <button
+          key={i}
+          onClick={() => handlePageChange(i)}
+          className={currentPage === i ? styles.current : ""}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    return paginationNumbers;
+  };
+
+  const startIdx = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIdx = startIdx + ITEMS_PER_PAGE;
+  const currentItems = searchResult.slice(startIdx, endIdx);
+
   const handleButtonClick = (index) => {
     setActiveButton(index === activeButton ? null : index);
   };
@@ -52,6 +94,7 @@ function Festival() {
       item.title.includes(search)
     );
 
+    setCurrentPage(1);
     setActiveButton(-1);
     setSearchResult(searchFestival);
     setSearch("");
@@ -66,19 +109,32 @@ function Festival() {
 
       setSearchResult(guResult);
     }
+
+    setCurrentPage(1);
   };
 
   const API_KEY =
     "hvQIg3vORMil2YY1vocfytMyq%2FmkvAvPIS3M5vn0K%2FWYFB7Lq6JV3mOBS0l1jYNs0P3j3cvR89wR592jBTBXxA%3D%3D";
   useEffect(() => {
+    window.scroll({ top: 0, behavior: "smooth" });
+    if (localStorage.getItem("festival")) {
+      const localFestival = JSON.parse(localStorage.getItem("festival"));
+      setFestival(localFestival);
+      setSearchResult(localFestival);
+      return;
+    }
+
     fetch(
       `http://apis.data.go.kr/B551011/KorService1/searchFestival1?serviceKey=${API_KEY}&_type=json&MobileOS=ETC&MobileApp=festival&areaCode=1&eventStartDate=20230101&eventEndDate=20241231&numOfRows=257`
     )
       .then((data) => data.json())
-      .then((festival) => {
-        setFestival(festival.response.body.items.item);
-        setSearchResult(festival.response.body.items.item);
-        setLoading(false);
+      .then((fe) => {
+        setFestival(fe.response.body.items.item);
+        setSearchResult(fe.response.body.items.item);
+        localStorage.setItem(
+          "festival",
+          JSON.stringify(fe.response.body.items.item)
+        );
       });
   }, []);
 
@@ -86,85 +142,104 @@ function Festival() {
     <div className={styles.main}>
       <div className={styles.back4}>
         <div>
-          {loading ? (
-            <div className={styles.main2}>
-              <p>로딩중...</p>
-            </div>
-          ) : (
-            <div className={styles.back1}>
-              <h1 onClick={() => categoryGu(-1)}>축제</h1>
-              <form onSubmit={onSubmit}>
-                <input
-                  type="text"
-                  value={search}
-                  onChange={onChange}
-                  placeholder="가고싶은 축제를 찾아보세요!"
-                  className={styles.search}
-                />
-                <button type="submit" className={styles.searchBtn}>
-                  검색
-                </button>
-              </form>
-            </div>
-          )}
+          <div className={styles.back1}>
+            <h1 onClick={() => categoryGu(-1)}>축제</h1>
+            <form onSubmit={onSubmit}>
+              <input
+                type="text"
+                value={search}
+                onChange={onChange}
+                placeholder="가고싶은 축제를 찾아보세요!"
+                className={styles.search}
+              />
+              <button type="submit" className={styles.searchBtn}>
+                검색
+              </button>
+            </form>
+          </div>
         </div>
 
         <div className={styles.back}>
-          {loading ? null : (
-            <div>
-              <div className={styles.subTitle}>
-                <b>시 / 군 / 구</b>
-                <span>Category</span>
-              </div>
-              <div className={styles.categoryBox}>
-                <ul>
-                  {Gu.map((item, idx) => (
-                    <li
-                      onClick={() => {
-                        categoryGu(idx - 1);
-                        handleButtonClick(idx - 1);
-                      }}
-                      className={`${styles.categoryItem} ${
-                        activeButton === idx - 1 ? styles.activeButton : ""
-                      }`}
-                      key={idx}
-                    >
-                      <button type="button">{item}</button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className={styles.back2}>
-                <h3>🔎  전체 결과 : {searchResult.length} 개</h3>
-              </div>
-              {searchResult.length > 0 ? (
-                <div className={styles.back2}>
-                  {searchResult.map(
-                    (item, idx) =>
-                      item.firstimage !== "" && (
-                        <div key={idx} className={styles.item}>
-                          <Link
-                            to={`/festival/${item.contentid}`}
-                            state={{ item }}
-                          >
-                            <img
-                              src={item.firstimage}
-                              alt="대표이미지 없음"
-                              className={styles.image}
-                            />
-                            <div className={styles.festivalName}>
-                              {item.title}
-                            </div>
-                          </Link>
-                        </div>
-                      )
-                  )}
-                </div>
-              ) : (
-                <div className={styles.noResult}>검색 결과가 없습니다...</div>
-              )}
+          <div>
+            <div className={styles.subTitle}>
+              <b>시 / 군 / 구</b>
+              <span>Category</span>
             </div>
-          )}
+            <div className={styles.categoryBox}>
+              <ul>
+                {Gu.map((item, idx) => (
+                  <li
+                    onClick={() => {
+                      categoryGu(idx - 1);
+                      handleButtonClick(idx - 1);
+                    }}
+                    className={`${styles.categoryItem} ${
+                      activeButton === idx - 1 ? styles.activeButton : ""
+                    }`}
+                    key={idx}
+                  >
+                    <button type="button">{item}</button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className={styles.back2}>
+              <h3>🔎 전체 결과 : {searchResult.length} 개</h3>
+            </div>
+
+            {searchResult.length > 0 ? (
+              <div className={styles.back2}>
+                {currentItems.map((item, idx) => (
+                  <Link to={`/festival/${item.contentid}`} state={{ item }}>
+                    <div key={idx} className={styles.item}>
+                      {item.firstimage === "" ? (
+                        <img
+                          src={placeholderImage}
+                          alt={item.title}
+                          className={styles.image}
+                        />
+                      ) : (
+                        <img
+                          src={item.firstimage}
+                          alt={item.title}
+                          className={styles.image}
+                        />
+                      )}
+
+                      <div className={styles.festivalName}>{item.title}</div>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            ) : (
+              <div className={styles.noResult}>검색 결과가 없습니다...</div>
+            )}
+
+            {totalPageCount > 0 && (
+              <div className={styles.pagination}>
+                <div className={styles.nav_buttons}>
+                  <button onClick={() => handlePageChange(1)}>&lt;&lt;</button>
+                  <button
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}
+                  >
+                    &lt;
+                  </button>
+                  {renderPaginationNumbers()}
+                  <button
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    disabled={currentPage === totalPageCount}
+                  >
+                    &gt;
+                  </button>
+                  <button onClick={() => handlePageChange(totalPageCount)}>
+                    &gt;&gt;
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/components/FestivalDetail.js
+++ src/components/FestivalDetail.js
@@ -1,14 +1,36 @@
-import { React } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { React, useEffect, useState } from "react";
+import { useLocation, Link } from "react-router-dom";
 import styles from "../css/FestivalDetail.module.css";
 
 function Detail() {
   const location = useLocation();
-  const histoty = useNavigate();
   const festival = location.state.item;
-  const goBack = () => {
-    histoty(-1);
+  const [detail, setDetail] = useState([]);
+  const [activeButton, setActiveButton] = useState(0);
+  const [first, setFirst] = useState(true);
+  const [second, setSecond] = useState(false);
+  const [third, setThird] = useState(false);
+
+  const placeholderImage =
+    "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg";
+
+  const handleButtonClick = (index) => {
+    if (index === 0) {
+      setFirst(true);
+      setSecond(false);
+      setThird(false);
+    } else if (index === 1) {
+      setFirst(false);
+      setSecond(true);
+      setThird(false);
+    } else {
+      setFirst(false);
+      setSecond(false);
+      setThird(true);
+    }
+    setActiveButton(index === activeButton ? index : index);
   };
+
   const formatDate = (dateString) => {
     const year = dateString.substr(0, 4);
     const month = dateString.substr(4, 2);
@@ -17,40 +39,158 @@ function Detail() {
     return `${year}년 ${month}월 ${day}일`;
   };
 
+  const API_KEY =
+    "hvQIg3vORMil2YY1vocfytMyq%2FmkvAvPIS3M5vn0K%2FWYFB7Lq6JV3mOBS0l1jYNs0P3j3cvR89wR592jBTBXxA%3D%3D";
+  useEffect(() => {
+    window.scroll({ top: 0, behavior: "smooth" });
+    fetch(
+      `	http://apis.data.go.kr/B551011/KorService1/detailIntro1?serviceKey=${API_KEY}&_type=json&MobileOS=ETC&MobileApp=festival&contentId=${festival.contentid}&contentTypeId=${festival.contenttypeid}`
+    )
+      .then((data) => data.json())
+      .then((detail) => {
+        setDetail(detail.response.body.items.item[0]);
+      });
+  }, [festival.contentid, festival.contenttypeid]);
+  console.log(detail)
   return (
     <div className={styles.main}>
       <div className={styles.back}>
         <div className={styles.imageBox}>
-          <img src={festival.firstimage} alt="이미지" />
+          {festival.firstimage === "" ? (
+            <img src={placeholderImage} alt="대체 이미지" />
+          ) : (
+            <img src={festival.firstimage} alt="이미지" />
+          )}
         </div>
 
         <div className={styles.contentBox}>
           <div>
             <h1>{festival.title}</h1>
-            
-            <table>
-              <tr>
-                <th>주소</th>
-                <td>{festival.addr1} {festival.addr2}</td>
-              </tr>
-              <tr>
-                <th>행사시작일</th>
-                <td>{formatDate(festival.eventstartdate)}</td>
-              </tr>
-              <tr>
-                <th>행사종료일</th>
-                <td>{formatDate(festival.eventenddate)}</td>
-              </tr>
-              <tr>
-                <th>전화번호</th>
-                <td>{festival.tel}</td>
-              </tr>
-            </table>
+
+            <div>
+              <ul className={styles.selectGroup}>
+                <li
+                  onClick={() => {
+                    handleButtonClick(0);
+                  }}
+                  className={`${activeButton === 0 ? styles.activeButton : ""}`}
+                >
+                  기본정보
+                </li>
+                <li
+                  onClick={() => {
+                    handleButtonClick(1);
+                  }}
+                  className={`${activeButton === 1 ? styles.activeButton : ""}`}
+                >
+                  이용안내
+                </li>
+                <li
+                  onClick={() => {
+                    handleButtonClick(2);
+                  }}
+                  className={`${activeButton === 2 ? styles.activeButton : ""}`}
+                >
+                  상세정보
+                </li>
+              </ul>
+            </div>
+
+            {first ? (
+              <div>
+                <table>
+                  <tr>
+                    <th>주소</th>
+                    <td>
+                      {festival.addr1} {festival.addr2}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>전화명</th>
+                    <td>{detail.sponsor1}</td>
+                  </tr>
+                  <tr>
+                    <th>전화번호</th>
+                    <td>
+                      {festival.tel === "" ? "연락처 없음" : festival.tel}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>홈페이지</th>
+                    <td>
+                      {detail.eventhomepage === "" ? "정보 없음" : <a href={detail.eventhomepage}>홈페이지 바로가기</a>}
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            ) : null}
+            {second ? (
+              <div>
+                <table>
+                  <tr>
+                    <th>행사시작일</th>
+                    <td>{formatDate(festival.eventstartdate)}</td>
+                  </tr>
+                  <tr>
+                    <th>행사종료일</th>
+                    <td>{formatDate(festival.eventenddate)}</td>
+                  </tr>
+                  <tr>
+                    <th>공연시간</th>
+                    <td>{detail.playtime}</td>
+                  </tr>
+                  <tr>
+                    <th>이용 요금</th>
+                    <td>
+                      {detail.usetimefestival === ""
+                        ? "무료"
+                        : detail.usetimefestival}
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            ) : null}
+            {third ? (
+              <div>
+                <table>
+                  <tr>
+                    <th>행사 장소</th>
+                    <td>{detail.eventplace}</td>
+                  </tr>
+                  <tr>
+                    <th>관람소요시간</th>
+                    <td>
+                      {detail.spendtimefestival === ""
+                        ? "소요시간 없음"
+                        : detail.spendtimefestival}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>주관사</th>
+                    <td>
+                      {detail.sponsor2 === ""
+                        ? detail.sponsor1
+                        : detail.sponsor2}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>주관사 연락처</th>
+                    <td>
+                      {detail.sponsor2tel === ""
+                        ? "연락처 없음"
+                        : detail.sponsor2tel}
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            ) : null}
           </div>
 
-          <div onClick={goBack} className={styles.backBtn}>
-            <div>목록으로</div>
-          </div>
+          <Link to={`/festival`} className={styles.backBtn}>
+            <div >
+              <div>목록으로</div>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
